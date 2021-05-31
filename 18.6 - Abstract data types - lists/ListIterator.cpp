32caaa9b886c8fa7dfbd74419376bffb3832122d
f0cusPie <64744700+f0cusPie@users.noreply.github.com>
2021-05-31 23:16:19 +0500

    Add files via upload

---
+++ 18.6 - Abstract data types - lists/ListIterator.cpp
@@ -0,0 +1,13 @@
+#include "ListIterator.h"
+
+void ListIterator::operator+=(int addValue)
+{
+	for (int i = 0; i < addValue; i++)
+		_pointedElement++;
+}
+
+void ListIterator::operator-=(int subValue)
+{
+	for (int i = 0; i < subValue; i--)
+		_pointedElement--;
+}
